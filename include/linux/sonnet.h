35228f5f599261f731878e9bcda2129c7b57981f
Daniel Palmer <daniel@0x0f.com>
2026-08-08 16:58:05 +0000

    pudding, basic UAPI for waiting on events from the kernel

--- include/linux/sonnet.h
+++ include/linux/sonnet.h
@@ -30,10 +30,35 @@ struct sonnet_boot {
 	__u32 reserved[2];
 };
 
+/*
+ * Events that the kernel wants us to service
+ */
+#define SONNET_EV_STATE		1
+
+struct sonnet_event {
+	__u16 type;
+	__u16 reserved[3];
+	union {
+		__u32 new_state;
+	};
+};
+
+/*
+ * kernel -> sonnetd interface for virtio
+ */
+struct sonnet_wait {
+	/* user pointer to an array of struct sonnet_event */
+	__u64 events;
+	__u32 nevents;
+	/* How long to wait if there are no events ready to return, 0 == no wait */
+	__u32 timeout_ms;
+};
+
 #define SONNET_IOC_MAGIC	'S'
 
 #define SONNET_GET_INFO		_IOR(SONNET_IOC_MAGIC, 0, struct sonnet_info)
 #define SONNET_STOP		_IO(SONNET_IOC_MAGIC, 1)
 #define SONNET_BOOT		_IOW(SONNET_IOC_MAGIC, 2, struct sonnet_boot)
+#define SONNET_WAIT		_IOWR(SONNET_IOC_MAGIC, 3, struct sonnet_wait)
 
 #endif /* _LINUX_SONNET_H */
